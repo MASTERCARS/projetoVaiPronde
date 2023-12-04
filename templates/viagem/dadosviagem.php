943a9d3e9c06092caf4ebffc322efadd536c3763
Vítor Dourado <vitorsouzadourado9@gmail.com>
2023-12-04 13:53:53 -0300

    autualização comitis

---
+++ templates/viagem/dadosviagem.php
@@ -0,0 +1,52 @@
+<?php
+
+$viagem = [
+    [
+        "promocao"=>"promoção",
+        "preco"=>1500,
+        "pacotes"=>"Salvador",
+        "foto"=>"imagem/img2.jpg"
+    ],
+    [
+        "promocao"=>"promoção",
+        "preco"=>1500,
+        "pacotes"=>"Ilhéus",
+        "foto"=>"imagem/img3.jpg"
+    ],
+    [
+        "promocao"=>"promoção",
+        "preco"=>1500,
+        "pacotes"=>"Rio de Contas",
+        "foto"=>"imagem/img4.jpg"
+    ],
+    [
+        "promocao"=>"promoção",
+        "preco"=>1500,
+        "pacotes"=>"Valença",
+        "foto"=>"imagem/img5.jpg"
+    ],
+    [
+        "promocao"=>"promoção",
+        "preco"=>1500,
+        "pacotes"=>"4 noites",
+        "foto"=>"imagem/imagen6.jpg"
+    ],
+    [
+        "promocao"=>"promoção",
+        "preco"=>1500,
+        "pacotes"=>"4 noites",
+        "foto"=>"imagem/imagen7.jpg"
+    ],
+    [
+        "promocao"=>"promoção",
+        "preco"=>1500,
+        "pacotes"=>"Bom Jesus da Lapa",
+        "foto"=>"imagem/imagen8.jpg" 
+    ],
+    [
+    "promocao"=>"promoção",
+    "preco"=>1500,
+    "pacotes"=>"Porto Seguro",
+    "foto"=>"imagem/img1.jpg"
+    ],
+    ];
