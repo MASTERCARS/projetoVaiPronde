897a6f7baa17f4e514b5194b6b922f17e3fc400f
Vítor Dourado <vitorsouzadourado9@gmail.com>
2023-12-05 09:50:00 -0300

    falata apenas arrumar o erro do viagemdetalhe

--- app/Views/viagemdetalhe.view.php
+++ app/Views/viagemdetalhe.view.php
@@ -21,7 +21,7 @@ $f = $viagem[$indice];
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/estilo2.css">
+    <link rel="stylesheet" href="<?=css('estilo2')?>">
     <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Nova+Flat&family=Roboto&display=swap" rel="stylesheet">
