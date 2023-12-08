5cc8750164845d73dffe9c205ac700c596202dc3
Vítor Dourado <vitorsouzadourado9@gmail.com>
2023-12-08 10:44:23 -0300

    atualização teste

--- app/rotas.php
+++ app/rotas.php
@@ -10,12 +10,10 @@ Router::add('/viagem','HomeController','viagem');
 Router::add('/sobre','HomeController','sobre');
 Router::add('/motoristaapp','HomeController','motoristaapp');
 Router::add('/viagemdetalhe','HomeController','viagemdetalhe');
-<<<<<<< HEAD
 Router::add('/configbd','HomeController','configbd');
 Router::add('/financeiro','HomeController','financeiro');
 Router::add('/standby','HomeController','standby');
 Router::add('/perfil','HomeController','perfil');
 Router::add('/vinculo','HomeController','vinculo');
-=======
+Router::add('/testar','HomeController','testar');
 
->>>>>>> 897a6f7baa17f4e514b5194b6b922f17e3fc400f
